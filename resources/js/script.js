a5c46659acf0d38095a1c434b711127831968126
Trlaic <igor@ss.com>
2021-02-19 14:35:42 +0100

    Animations added

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,5 +9,23 @@ $(document).ready(function(){
     }, {
         offset:'60px'
     });
+    $('.section-offer').waypoint(function(direction){
+        $('.ponuda').addClass('animate__animated animate__fadeIn');
+    });
+    $('.section-works').waypoint(function(direction){
+        $('.logo-2').addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset:'30%'
+    });
+    $('.section-cities').waypoint(function(direction){
+        $('.gradovi').addClass('animate__animated animate__fadeIn');
+    },{
+        offset: '30%'
+    });
+    $('.section-accounts').waypoint(function(direction){
+        $('.premium').addClass('animate__animated animate__pulse');
+    },{
+        offset:'30%'
+    });
 
 });
